6e853e4e6aa34c823b6d0e1ce599f2473e338c1a
thomas.toledo <thomas.toledo@eemi.com>
2021-03-15 02:10:08 +0100

    let's go

--- src/app/shared/services/student.service.spec.ts
+++ src/app/shared/services/student.service.spec.ts
@@ -2,7 +2,6 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
-import { HttpClient } from '@angular/common/http';
 
 import { TestBed } from '@angular/core/testing';
 
